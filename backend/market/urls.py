4052a81071ae9a083f211bccc34bad7a87896a1e
Arseniy Kushnir <arseniykushnir2007@gmail.com>
2026-02-28 14:00:31 +0200

    added http_origin view

--- backend/market/urls.py
+++ backend/market/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from market.views import ProductViewSet, OrderViewSet, SignboardViewSet
+from market.views import ProductViewSet, OrderViewSet, SignboardViewSet, CorsViewSet
 
 router = DefaultRouter()
 router.register("products", ProductViewSet)
 router.register("signboards", SignboardViewSet)
 router.register("orders", OrderViewSet)
+router.register("check_cors", CorsViewSet)
 
 urlpatterns = router.urls
 
--- backend/market/views.py
+++ backend/market/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets, mixins
-from market.models import Product, Signboard, Order
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from market.models import Product, Signboard, Order, Review
 from market.permissions import IsAdminOrReadOnly
 from market.serializers import ProductSerializer, SignboardSerializer, OrderSerializer
 
@@ -44,3 +46,14 @@ class OrderViewSet(
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class CorsViewSet(viewsets.GenericViewSet):
+    queryset = Review.objects
+    lookup_field = "param"
+
+    def list(self, request, *args, **kwargs):
+        return Response({"data": f"{request.META.get("HTTP_ORIGIN")}"})
+
+    def retrieve(self, request, *args, **kwargs):
+        return Response({"data": f"{request.META.get(kwargs["param"])}"})
