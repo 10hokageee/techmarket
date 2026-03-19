e038f0c0c379d973fccb742ccc7f41d508a16a02
Arseniy Kushnir <arseniykushnir2007@gmail.com>
2026-03-19 12:12:24 +0200

    Added local development related files to .gitignore and changed the server startup file

--- backend/build.sh
+++ backend/build.sh
@@ -2,10 +2,9 @@
 
 if [ -f .env ]; then
   set -a
-  source .env
+  source <(sed 's/\r//' .env)
   set +a
 fi
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
-#python manage.py qluster &
-gunicorn --workers 1 --threads 2 --bind 0.0.0.0:${PORT:-10000} techmarketAPI.wsgi:application
+gunicorn --workers 1 --threads 2 --bind 0.0.0.0:${PORT:-10000} techmarketAPI.wsgi:application & python manage.py qcluster
