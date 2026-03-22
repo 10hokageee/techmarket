a731031c9409ebe42e3232d6a486c3cbe9d7b026
Nikita Serdyukov <nikita.serdyukov.20033002@gmail.com>
2026-03-23 00:22:33 +0200

    add loader

---
+++ frontend/src/components/Loader/Loader.tsx
@@ -0,0 +1,5 @@
+export const Loader = () => (
+  <div className="fixed inset-0 z-[9999] flex items-center justify-center w-full h-full bg-white/50">
+    <div className="w-[2em] h-[2em] border-[0.3em] border-gray-200 border-l-black rounded-full animate-spin" />
+  </div>
+);
