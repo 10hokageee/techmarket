0b9d37ac27afa47011895f07c05e49d651bd23c4
Arseniy Kushnir <arseniykushnir2007@gmail.com>
2026-03-29 21:48:22 +0300

    Created a text display of analytics for the tester

--- backend/analytics/views.py
+++ backend/analytics/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.db.models import Q
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.status import HTTP_200_OK
 
-# Create your views here.
+from analytics.models import SessionParameters
+
+
+class SessionParametersDevView(APIView):
+    def get(self, request):
+        queryset = SessionParameters.objects.select_related("user").filter(
+            (
+                ~(Q(device="UNKNOWN") | Q(device="BOT"))
+                & ~Q(browser="OTHER")
+                & ~Q(continent="NORTH AMERICA")
+                & ~Q(country="US")
+            )
+        )
+        result = [
+            {
+                "id": obj.pk,
+                "started_at": obj.started_at,
+                "device": obj.device,
+                "browser": obj.browser,
+                "continent": obj.continent,
+                "country": obj.country.name,
+                "countryCode": obj.country.code,
+                "user-username": obj.user.username if obj.user else None,
+            }
+            for obj in queryset
+        ]
+        return Response(status=HTTP_200_OK, data=result)
