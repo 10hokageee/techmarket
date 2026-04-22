aeaee42fb6d8a2b3137d917ecaa5edc0220cdec7
Nikita Serdyukov <134019773+10hokageee@users.noreply.github.com>
2026-04-22 20:34:33 +0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     server: {
     allowedHosts: [
         'devices-catalog.onrender.com',
-        'techmarket-1eif.onrender.com'
+        'techmarket-shop.onrender.com'
     ],
   }
 });
