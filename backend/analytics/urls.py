568784cdca9e98b41ce229eaa9257b77716ea1fc
Arseniy Kushnir <arseniykushnir2007@gmail.com>
2026-04-26 18:26:59 +0300

    Fix path error

--- backend/analytics/urls.py
+++ backend/analytics/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from analytics.views import TestApiView
+from analytics.views import CollectAnalyticsView
 
-urlpatterns = []
+urlpatterns = [
+    path("events/", CollectAnalyticsView.as_view()),
+]
 
 app_name = "analytics"
